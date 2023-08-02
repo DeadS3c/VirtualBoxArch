b81aa81376d94ae706203e149de1092639384142
DeadS3c <h4ck0rd@gmail.com>
2023-08-02 12:57:47 +0200

    Added packages

--- install_root.sh
+++ install_root.sh
@@ -77,7 +77,7 @@ pacman -S --noconfirm git virtualbox-guest-utils xcompmgr rxvt-unicode \
     ttf-jetbrains-mono-nerd ttf-joypixels xlockmore zsh openssh wget privoxy \
     yt-dlp zathura zathura-cb zathura-djvu zathura-pdf-mupdf libreoffice-fresh \
     tmux openbsd-netcat tcpdump perl-image-exiftool ranger ufw scrot flameshot \
-    zaproxy firefox chromium iwd dhclient docker go
+    zaproxy firefox chromium iwd dhclient docker go bind jq whois
 
 # Configuracion
 ## Display Manager
